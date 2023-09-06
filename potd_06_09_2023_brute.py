f4d1520e89e5069e0ad996fb471bf1547110ed3b
Asit kumar <97583597+asitkuma@users.noreply.github.com>
2023-09-06 19:03:01 +0530

    Create potd_06_09_2023_brute.py

---
+++ potd_06_09_2023_brute.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    #Function to find a Mother Vertex in the Graph.
+    sum1=1
+    def recur(self,l1,list2,adj):
+        for i in list2:
+            if l1[i]!=-1:
+                l1[i]=-1
+                Solution.sum1+=1
+                self.recur(l1,adj[i],adj)
+    
+    def findMotherVertex(self, V, adj):
+        l3=[]
+        for i in range(len(adj)):
+            visited=[1 for i in range(V)]
+            visited[i]=-1
+            self.recur(visited,adj[i],adj)
+            if Solution.sum1==V:
+                l3.append(i)
+                
+            Solution.sum1=1
+        if l3:
+            return l3[0]
+        return -1    
+
